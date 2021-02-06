5c4c6b5e8c5e43f2b635bc5c8ebd7faa1d133afd
simanto-das <53492679+simanto-das@users.noreply.github.com>
2021-02-06 17:56:35 +0600

    Add files via upload
    sports ecomerce website project

---
+++ SPORTS_ECOMERCE_FINAL/success.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.card {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+     background-color: #9999ff;
+  color: #000000;
+  padding: 10px;
+  text-align: center;
+
+   
+}
+
+
+	.header {
+  background-color: 
+ #ffcc00;
+  color: #ff751a;
+  padding: 25px;
+  text-align: center;
+
+}
+	</style>
+</head>
+<body>
+
+
+
+	<div class="header">
+  <h1 style="text-align:center">MY ORDER</h1>
+</div>
+
+<div class="card">
+
+  
+  <p>Order placed successfully.</p>
+  
+ 
+</div>
+
+
+
+</body>
+</html>
